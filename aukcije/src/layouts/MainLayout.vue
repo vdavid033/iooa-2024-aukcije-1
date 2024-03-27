c3d3db2ea878b03ccadf681a565b237843a67517
HSanel2000 <shalacevi@veleri.hr>
2024-03-27 15:00:46 +0100

    Sanel- napravio sam prijavu i odjavu da se prikazuju s desne strane toolbara, prikazuje se ovisno o tome je li korisnik prijavljen ili ne

--- aukcije/src/layouts/MainLayout.vue
+++ aukcije/src/layouts/MainLayout.vue
@@ -1,9 +1,8 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      
       <q-toolbar>
-          <q-btn
+        <q-btn
           flat
           dense
           round
@@ -12,37 +11,37 @@
           @click="toggleLeftDrawer"
         />
 
-      <q-toolbar-title>
-    <q-avatar>
-        <img src="~assets\aukcije_logo.jpg" alt="Logo">
-    </q-avatar>
-  </q-toolbar-title>
-<q-space></q-space>
-          
-      
-          <q-input
-            v-model="search"
-            filled
-            placeholder="Search"
-            dense
-            class="w-200"
-            @keyup.enter="searchItems"
-          />
-      
-          <q-btn
-            icon="search"
-            color="primary"
-            class="q-mr-md"
-            @click="searchItems"
-          />
-      
-          <q-btn
-            icon="clear"
-            color="primary"
-            @click="clearSearch"
-          />
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="~assets\aukcije_logo.jpg" alt="Logo">
+          </q-avatar>
+        </q-toolbar-title>
 
-          <div class="q-pa-md">
+        <q-space></q-space>
+
+        <q-input
+          v-model="search"
+          filled
+          placeholder="Pretraga"
+          dense
+          class="w-200"
+          @keyup.enter="searchItems"
+        />
+
+        <q-btn
+          icon="search"
+          color="primary"
+          class="q-mr-md"
+          @click="searchItems"
+        />
+
+        <q-btn
+          icon="clear"
+          color="primary"
+          @click="clearSearch"
+        />
+
+        <div class="q-pa-md">
             <q-btn-dropdown color="primary" :label="selectedCategory">
               <q-list>
                 <q-item clickable v-close-popup @click="onItemClick('Photos')">
@@ -65,67 +64,67 @@
               </q-list>
             </q-btn-dropdown>
           </div>
+        <q-btn
+          icon="refresh"
+          color="primary"
+          @click="refresh"
+        />
 
-          <q-btn
-            icon="refresh"
-            color="primary"
-            @click="refresh"
-          />
+        <q-space />
 
-         <q-space /><q-space /><q-space /><q-space /><q-space /><q-space /><q-space /><q-space />
-       <!-- <router-link to="/Moj_profil" class="link-style">
-          <q-btn label="My Profile" color="primary" class="q-mr-md" />
-        </router-link>-->
-        <router-link to="/Prijava_m" class="link-style">
+        <router-link v-if="!loggedIn" to="/prijava" class="link-style">
+          <q-btn label="Prijava" color="primary" class="q-mr-md" />
+        </router-link>
+        <router-link v-if="loggedIn" to="/" class="link-style">
           <q-btn label="Odjava" color="negative" class="q-mr-md" />
         </router-link>
-        </q-toolbar>
+
+      </q-toolbar>
     </q-header>
 
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <!-- Sadržaj lijevog drawer-a -->
       <q-list>
         <q-item-label header class="text-bold text-black">
           Mogućnosti
         </q-item-label>
         
         <div class="q-pa-sm col">
-          <!--        Za navigaciju bez otvaranja novog tab-a-->
           <div class="q-pa-sm col">
             <q-btn class="flex flex-center" style="width: 280px">
-              <router-link to="/prijava" class="link-style"
-                >Prijava
+              <router-link to="/prijava" class="link-style">
+                Prijava
               </router-link>
             </q-btn>
           </div>
           <div class="q-pa-sm col">
             <q-btn class="flex flex-center" style="width: 280px">
-              <router-link to="/registracija" class="link-style"
-                >Registracija
+              <router-link to="/registracija" class="link-style">
+                Registracija
               </router-link>
             </q-btn>
           </div>
           <div class="q-pa-sm col">
             <q-btn class="flex flex-center" style="width: 280px">
-              <router-link to="/" class="link-style"
-                >Početna stranica</router-link>
-            </q-btn>
-            
-          </div>
-          <div class="q-pa-sm col">
-            <q-btn class="flex flex-center" style="width: 280px">
-              <router-link to="postavi" class="link-style"
-                >Dodaj aukciju
+              <router-link to="/" class="link-style">
+                Početna stranica
               </router-link>
             </q-btn>
           </div>
           <div class="q-pa-sm col">
             <q-btn class="flex flex-center" style="width: 280px">
-              <router-link to="/Moj_profil" class="link-style"
-                >Moj profil
+              <router-link to="postavi" class="link-style">
+                Dodaj aukciju
               </router-link>
             </q-btn>
           </div>
-         
+          <div class="q-pa-sm col">
+            <q-btn class="flex flex-center" style="width: 280px">
+              <router-link to="/Moj_profil" class="link-style">
+                Moj profil
+              </router-link>
+            </q-btn>
+          </div>
         </div>
       </q-list>
     </q-drawer>
@@ -145,28 +144,25 @@ const baseUrl = "http://localhost:3306/api/";
 export default defineComponent({
   name: "MainLayout",
 
-  methods: {
-    handleClick() {
-      axios
-        .get(baseUrl + "all-predmet", {})
-        .then((response) => {
-          console.log(response);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
+  data() {
+    return {
+      leftDrawerOpen: false,
+      loggedIn: false,
+      search: "",
+      selectedCategory: "",
+    };
   },
 
-  setup() {
-    const leftDrawerOpen = ref(false);
+methods: {
+    // Metode za obradu događaja
 
-    return {
-      leftDrawerOpen,
-      toggleLeftDrawer() {
-        leftDrawerOpen.value = !leftDrawerOpen.value;
-      },
-    };
+    toggleLeftDrawer() {
+      this.leftDrawerOpen = !this.leftDrawerOpen;
+    },
+
+    refresh() {
+      // Implementirajte logiku za osvežavanje
+    },
   },
 });
 </script>
